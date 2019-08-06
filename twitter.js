d4d6c8b09c604c1f152574f7b358036ec791adff
maulidul arzaqi <maulidularzaqi@mauliduls-MacBook-Air.local>
2019-08-06 09:32:40 +0700

    update next token crawl twitter

--- twitter.js
+++ twitter.js
@@ -19,24 +19,88 @@ class twitter extends Core {
             .initMysql(['RippleClientKeyword'])
     }
 
+    options (maxPosition = '' , qsearch = '', value = ''){
+        
+        const options = { 
+            method: 'GET',
+            url: 'https://twitter.com/i/search/timeline',
+            qs: 
+                { vertical: 'default',
+                q: `${qsearch}`,
+                src: 'typd',
+                include_available_features: '1',
+                include_entities: '1',
+                // max_position: `${maxPosition}`,
+                reset_error_state: 'false' ,
+                latent_count: 362
+                },
+        }
+
+        if(value === 'max'){
+            options.qs.max_position = `${maxPosition}`
+            // options.qs.min_position =``
+        } else {
+            options.qs.min_position =`${maxPosition}`
+            // options.qs.max_position = ``
+        }
+
+        return options
+    }
+
+    NextPage (project = {} , minPosition = '') {
+        return new Promise((resolve, reject) => {
+            console.log('getting Next Post...')
+            const {keyword : qsearch, keyid, client, include, exclude} = project
+            let options = this.options (minPosition , qsearch)
+            request(options, async (error, response, body) =>{
+                console.log('next page')
+                try {
+                    if (error) console.log(error)
+                    body = JSON.parse(body)
+                    const maxPosition = body.max_position
+                    body = body.items_html
+                    body = body
+                        .replace(/\&quot;/img, '"')
+                        .replace(/\↵/g, ' ')
+                        .replace(/ +/g, ' ')
+                        .replace(/\&amp;/g, '&')
+                        .replace(/\&nbsp;/, ' ')
+                        .trim()
+                    let  dataValue =  body.split('<li class="js-stream-item stream-item stream-item')
+                    const dataTwitter = await this.mapper.mappingDataStreams(dataValue ,project)
+                    console.log({maxPosition})
+                    // this.db_adapter.saveStreamToCollection(dataTwitter).then()
+                    if(maxPosition){
+                        // await this.NextPage(project, maxPosition)
+                    }
+                    resolve()
+                } catch (err){
+                    console.log(err)
+                    reject(err)
+                }
+            })
+
+        })
+    }
+
     doSearching (options = {}) {
         return new Promise((resolve, reject) => {
-
-            const {qsearch, keyid, client, include, exclude} = options
+            console.log('getting Post...')
+            const {qsearch, keyid, client, include, exclude, keyword} = options
             const patch = `${Math.random()}`.substr(0, 5)
             const opt = {
                 method: 'GET',
                 url,
                 headers: {},
                 qs:{
-                    'q':'indofood',
+                    'q': encodeURI(qsearch),
                     'src':'typd'
                 }
             }
             let project = {
                 id: keyid,
                 client,
-                keyword: qsearch,
+                keyword,
                 include,
                 exclude
             }
@@ -46,21 +110,9 @@ class twitter extends Core {
                 maxPosition = result(maxPosition, '[0]', '')
                     .replace('data-max-position="', '')
                     .replace('"', '')
-                    
-                    var options = { 
-                        method: 'GET',
-                        url: 'https://twitter.com/i/search/timeline',
-                        qs: 
-                            { vertical: 'default',
-                            q: 'indomie',
-                            src: 'typd',
-                            include_available_features: '1',
-                            include_entities: '1',
-                            max_position: 'thGAVUV0VFVBYBFoLAup2TsMWSIBIYtAESY8LrAAAB9D-AYk3S8an8AAAAFBASgot8F6ACEBH5IZnWwAEQEon2_VdQABASeB2S1sABEBKlJ12WwAAQEnIUbtRgABAOTe1_FBACEBKbk0xVUAAQEpe3i5bgABASisCZ11ABEBKQS5zWwAYQEqisG5bgABASjTkll6AAEA-Uvb1XoAAQDwaUKVVQBBASSFobVCAAEBIobQuUIAEQEm0gE5eQABAOdi9IF7ABEA59pn7VUAAVAhUAFQAlAFUAFQAA',
-                            reset_error_state: 'false' 
-                            },
-                    }
-                    request(options, (error, response, body) =>{
+
+                   let options = this.options (maxPosition , qsearch , 'max')
+                    request(options, async (error, response, body) => {
                         try {
                             if (error) console.log(error)
                             body = JSON.parse(body)
@@ -69,12 +121,21 @@ class twitter extends Core {
                             body = body
                                 .replace(/\&quot;/img, '"')
                                 .replace(/\↵/g, ' ')
+                                .replace(/\n/g, ' ')
                                 .replace(/ +/g, ' ')
                                 .replace(/\&amp;/g, '&')
                                 .replace(/\&nbsp;/, ' ')
                                 .trim()
+
                             let  dataValue =  body.split('<li class="js-stream-item stream-item stream-item')
-                            this.mapper.mapingDataStreams(dataValue ,project)
+                            console.log(`total Post ${dataValue.length}`)      
+                            const dataTwitter = await this.mapper.mappingDataStreams(dataValue ,project)
+                            this.db_adapter.saveStreamToCollection(dataTwitter).then((r) => {
+                                console.log(r)
+                            }).catch(console.log)
+                            if(minPosition){
+                            //    await this.NextPage(project, minPosition)
+                            }
                             resolve()
                         } catch (err) {
                             console.log(err)
@@ -85,12 +146,9 @@ class twitter extends Core {
         })
     }
 
-    start (keyids = '') {
-        const ids = []
-        keyids = keyids.split(',')
-        for (const id of keyids){
-            ids.push(parseInt(id))
-        }
+    start () {
+        const keyids = this.keyids.split(',')
+        const ids = keyids.map(x => parseInt(x)).filter(x => x > 0)
         this.db_adapter.getClientKeyword(ids)
             .then(async (keywordData) =>{
                 for(const key of keywordData){
@@ -99,13 +157,19 @@ class twitter extends Core {
                         .map(x => x.trim())
                         .filter(x => x.length > 0)
                         .map(x => ({
-                            qsearch: x,
+                            keyword: x,
+                            qsearch: [
+                                x,
+                                this.since,
+                                this.until
+                            ].join(' '),
                             keyid: key.keyId,
                             client: key.clientId,
                             include: key.KeyInclude,
                             exclude: key.KeyExclude
                         }))
                     for(const opt of options){
+                        console.log(opt)
                         await this.doSearching(opt)
                     }
                 }
@@ -113,6 +177,17 @@ class twitter extends Core {
             })
             .catch(console.error)
     }
+
+    setQuery(rootConfig = {}) {
+        this.keyids = rootConfig['keyids']
+        this.since = rootConfig['since'] ? 'since:' + rootConfig['since'] : ''
+        this.until = rootConfig['until'] ? 'until:' + rootConfig['until'] : ''
+        return this
+    }
 }
 
-new twitter().start('55')
+new twitter().setQuery({
+    keyids: '55',
+    since: '2019-07-01',
+    until: '2019-07-05'
+}).start()
