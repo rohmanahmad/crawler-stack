2dee7cc19ca576b10490a8089d8b961b1a747f29
rohmanmail@gmail.com <rohmanahmad>
2019-09-03 23:23:33 +0700

    update

--- src/Servers/Public/trends/assets/js-modules/twitter-trend-chart.js
+++ src/Servers/Public/trends/assets/js-modules/twitter-trend-chart.js
@@ -1,9 +1,9 @@
 'use strict'
 
-$(document).ready(function () {
+const paintChartTwitter = function () {
     const datasets = twitterData['items']
     const labels = twitterData['labels']
-    const ctx = document.getElementById('ch5').getContext('2d');
+    const ctx = document.getElementById('ch5-twitter').getContext('2d');
     var myBubbleChart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -18,4 +18,29 @@ $(document).ready(function () {
             }
         }
     })
+}
+
+const paintChartYoutube = function () {
+    const datasets = youtubeData['items']
+    const labels = youtubeData['labels']
+    const ctx = document.getElementById('ch5-youtube').getContext('2d');
+    var myBubbleChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels,
+            datasets
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    })
+}
+
+$(document).ready(function () {
+    paintChartTwitter()
+    paintChartYoutube()
 })
