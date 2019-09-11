e99a4728dfc81b3d78d93383157fb68951916e42
rohmanmail@gmail.com <rohmanahmad>
2019-09-12 00:44:20 +0700

    add profile id detail

--- src/App/Services/IGProfiles.js
+++ src/App/Services/IGProfiles.js
@@ -30,12 +30,16 @@ class IGProfiles {
         return new Promise((resolve, reject) => {
             const url = profileUrl
                 .replace('<username>', username)
+            console.log('[GET]', url)
             request
                 .get(url, function (err, res, body) {
                     if (err) return reject(err)
                     try {
+                        if (res.statusCode !== 200) {
+                            return resolve({})
+                        }
                         const json = JSON.parse(body)
-                        return resolve(json)
+                        return resolve(result(json, 'graphql.user', {}))
                     } catch (err) {
                         reject(err)
                     }
